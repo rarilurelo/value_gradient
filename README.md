d59857537ec9667f23159d6e6b17162fed047fb8
rarilurelo <rarilurelo@gmail.com>
2017-04-04 11:48:56 +0900

    rmove codes

---
+++ README.md
@@ -0,0 +1,21 @@
+# Value gradient methods
+
+This repository is reproduction of value gradient methods such as [DDPG](https://arxiv.org/pdf/1509.02971v5.pdf), [SVG](https://arxiv.org/pdf/1510.09142.pdf), [SoftQ](https://arxiv.org/pdf/1702.08165.pdf).
+
+- [x] DDPG
+- [x] SVG(0)
+- [ ] SoftQ
+
+
+## Requirement
+* tensorflow 1.0.1
+* keras 2.0.2
+* Python 3.5.2
+* gym 0.7.2
+* rllab 0.1.0
+
+
+## Usage
+```
+python run_ddpg.py
+```
--- run_soft_q.py
+++ run_soft_q.py
@@ -10,22 +10,13 @@ from soft_q import SoftQ
 from replay_buffer import SimpleReplayPool
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--pi_lr', type=float, default=7e-4)
-parser.add_argument('--gamma', type=float, default=1)
-parser.add_argument('--epoch', type=int, default=1000)
 parser.add_argument('--log', type=str, default='log_soft_q')
 args = parser.parse_args()
 
-#log_dir = args.log
-#
-#if not os.path.exists(log_dir):
-#    os.mkdir(log_dir)
-#env = gym.make('Pendulum-v0')
 env = GymEnv('Pendulum-v0', record_video=True, log_dir=args.log)
 env_spec = dict(
         action_space=env.action_space,
         observation_space=env.observation_space)
-#env = wrappers.Monitor(env, log_dir, force=True)
 
 net = StochasticNet(env_spec)
 
